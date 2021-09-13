1436265a86759992148602501d1ced7f94e66104
shawn-huesman <mrshawnhuesman@gmail.com>
2021-09-12 22:59:31 -0400

    initial commit

---
+++ app/src/main/java/com/example/csc415_hw1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.csc415_hw1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setTitle("Happy Birthday")
+    }
+}
